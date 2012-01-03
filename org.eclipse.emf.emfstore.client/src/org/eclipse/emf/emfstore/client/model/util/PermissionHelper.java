db8b917d5fc2090f842309fab1511f98419c5be3
André Böhlke <a.boehlke@gmail.com>
2012-01-03 14:56:29 +0100

    support checking operation permissions

--- org.eclipse.emf.emfstore.client/src/org/eclipse/emf/emfstore/client/model/util/PermissionHelper.java
+++ org.eclipse.emf.emfstore.client/src/org/eclipse/emf/emfstore/client/model/util/PermissionHelper.java
@@ -3,11 +3,23 @@ package org.eclipse.emf.emfstore.client.model.util;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 
 import org.eclipse.emf.emfstore.client.model.impl.UsersessionImpl;
 import org.eclipse.emf.emfstore.server.EmfStore;
+import org.eclipse.emf.emfstore.server.accesscontrol.Permission;
 import org.eclipse.emf.emfstore.server.model.SessionId;
+import org.eclipse.emf.emfstore.server.model.accesscontrol.ACGroup;
+import org.eclipse.emf.emfstore.server.model.accesscontrol.ACOrgUnit;
+import org.eclipse.emf.emfstore.server.model.accesscontrol.ACUser;
+import org.eclipse.emf.emfstore.server.model.accesscontrol.PermissionSet;
+import org.eclipse.emf.emfstore.server.model.accesscontrol.PermissionType;
+import org.eclipse.emf.emfstore.server.model.accesscontrol.RoleAssignment;
 
 /**
  * helps when checking if the current user has permissions for an operation
@@ -67,6 +79,51 @@ public class PermissionHelper {
 	 */
 	public PermissionHelper(SessionId sessionId) {
 		this.emfStore = UsersessionImpl.getEmfStoreProxy(sessionId);
+	}
+
+	public static boolean hasPermissions(ACUser user, Permission[] permissions, PermissionSet permissionSet) {
+
+		Set<Permission> requiredPermissions = new HashSet<Permission>();
+		requiredPermissions.addAll(Arrays.asList(permissions));
+
+		for (Permission permission : getPermissions(user)) {
+			String projectId = permission.getProjectId();
+			Iterator<Permission> iterator = requiredPermissions.iterator();
+			while (iterator.hasNext()) {
+				Permission requiredPermission = iterator.next();
+				if (requiredPermission.getId().equals(permission.getId())
+					&& (projectId == null || projectId.equals(requiredPermission.getProjectId()))) {
+					iterator.remove();
+				}
+			}
+
+			if (requiredPermissions.size() == 0) {
+				break;
+			}
+		}
+
+		if (requiredPermissions.size() == 0) {
+			return true;
+		}
+
+		return false;
+	}
+
+	private static Collection<Permission> getPermissions(ACOrgUnit orgUnit) {
+		Set<Permission> permissions = new HashSet<Permission>();
+
+		for (RoleAssignment assignment : orgUnit.getRoles()) {
+			for (PermissionType permissionType : assignment.getRole().getPermissionTypes()) {
+				String projectId = assignment.getProjectId() == null ? null : assignment.getProjectId().getId();
+				permissions.add(new Permission(permissionType.getId(), projectId));
+			}
+		}
+
+		for (ACGroup group : orgUnit.getGroups()) {
+			permissions.addAll(getPermissions(group));
+		}
+
+		return permissions;
 	}
 
 }
