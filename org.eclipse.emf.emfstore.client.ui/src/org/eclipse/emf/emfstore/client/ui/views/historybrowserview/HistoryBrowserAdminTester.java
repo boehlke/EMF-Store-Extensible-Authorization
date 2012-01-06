4f0ae6f1313513930df562383e3bc0640d21eaf8
André Böhlke <a.boehlke@gmail.com>
2012-01-06 17:20:35 +0100

    fixed property testers

--- org.eclipse.emf.emfstore.client.ui/src/org/eclipse/emf/emfstore/client/ui/views/historybrowserview/HistoryBrowserAdminTester.java
+++ org.eclipse.emf.emfstore.client.ui/src/org/eclipse/emf/emfstore/client/ui/views/historybrowserview/HistoryBrowserAdminTester.java
@@ -12,8 +12,12 @@ package org.eclipse.emf.emfstore.client.ui.views.historybrowserview;
 
 import org.eclipse.core.expressions.PropertyTester;
 import org.eclipse.emf.emfstore.client.model.ProjectSpace;
-import org.eclipse.emf.emfstore.client.ui.commands.IsAdminTester;
 import org.eclipse.emf.emfstore.client.ui.views.scm.SCMTreeNode;
+import org.eclipse.emf.emfstore.client.ui.views.users.UserUiController;
+import org.eclipse.emf.emfstore.server.connection.xmlrpc.util.StaticOperationFactory;
+import org.eclipse.emf.emfstore.server.exceptions.InvalidInputException;
+import org.eclipse.emf.emfstore.server.model.versioning.TagVersionSpec;
+import org.eclipse.emf.emfstore.server.model.versioning.VersioningFactory;
 
 /**
  * Tests if the user has admin permissions based on a HistoryInfo node.
@@ -21,6 +25,11 @@ import org.eclipse.emf.emfstore.client.ui.views.scm.SCMTreeNode;
  * @author shterev
  */
 public class HistoryBrowserAdminTester extends PropertyTester {
+
+	private static final Object PROP_CAN_REMOVE_TAG = "canRemoveTag";
+	private static final Object PROP_CAN_ADD_TAG = "canAddTag";
+	private static final Object PROP_CAN_REVERT = "canRevert";
+	private static final Object PROP_CAN_FORCE_REVERT = "canForceRevert";
 
 	/**
 	 * {@inheritDoc}
@@ -35,8 +44,35 @@ public class HistoryBrowserAdminTester extends PropertyTester {
 			if (projectSpace == null) {
 				return false;
 			}
-			IsAdminTester tester = new IsAdminTester();
-			return tester.test(projectSpace, property, args, expectedValue);
+
+			if (property.equals(PROP_CAN_REMOVE_TAG)) {
+				TagVersionSpec tag = VersioningFactory.eINSTANCE.createTagVersionSpec();
+				try {
+					// TODO: this should be called with the real tag which would be removed
+					return UserUiController.getInstance().canExecute(
+						StaticOperationFactory.createRemoveTagOperation(projectSpace.getProjectId(), projectSpace
+							.getProjectInfo().getVersion(), tag));
+				} catch (InvalidInputException e) {
+					// TODO Auto-generated catch block
+					return false;
+				}
+			} else if (property.equals(PROP_CAN_ADD_TAG)) {
+				TagVersionSpec tag = VersioningFactory.eINSTANCE.createTagVersionSpec();
+				try {
+					return UserUiController.getInstance().canExecute(
+						StaticOperationFactory.createAddTagOperation(projectSpace.getProjectId(), projectSpace
+							.getProjectInfo().getVersion(), tag));
+				} catch (InvalidInputException e) {
+					// TODO Auto-generated catch block
+					return false;
+				}
+			} else if (property.equals(PROP_CAN_FORCE_REVERT)) {
+				// TODO
+				return true;
+			} else if (property.equals(PROP_CAN_REVERT)) {
+				// TODO
+				return true;
+			}
 		}
 		return false;
 	}
